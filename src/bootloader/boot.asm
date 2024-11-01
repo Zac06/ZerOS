3bed02c2f794e7e717794765e692532a951dc460
Zaccaria <zaccaria.francescato@gmail.com>
2024-11-01 21:41:35 +0100

    added reading function

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -49,24 +49,42 @@ start:
 puts:
     push si
     push ax
+    push bx
 
-.puts_loop:
-    lodsb                   ;load char in AL (8 bits, hence ASCII)
-    or al, al               ;check if null: if 0flag is set, then it's null
-    jz .puts_done
+    .puts_loop:
+        lodsb                   ;load char in AL (8 bits, hence ASCII)
+        or al, al               ;check if null: if 0flag is set, then it's null
+        jz .puts_done
 
-    mov ah, 0x0e
-    mov bh, 0
-    int 0x10
+        mov ah, 0x0e
+        mov bh, 0
+        int 0x10
 
-    jmp .puts_loop
+        jmp .puts_loop
 
-.puts_done:
-    pop ax
-    pop si
-    ret                     ;return from where it was called
+    .puts_done:
+        pop bx
+        pop ax
+        pop si
+        ret                     ;return from where it was called
 
 ;================================================================================
+
+floppy_error:
+    mov si, msg_read_failed
+    call puts
+    jmp wait_key_and_reboot
+    
+    wait_key_and_reboot:
+        mov ah, 0
+        int 0x16            ;wait for key
+        jmp 0xFFFF          ;jump to beginning of BIOS, should reboot
+
+    .halt:
+        cli                 ;disable interrupts
+        hlt
+
+;===============================================================================
 
 main:
     ;setup data segments
@@ -78,20 +96,121 @@ main:
     mov ss, ax
     mov sp, 0x7c00          ;stack grows down from the start of the program
 
+    ;read from floppy
+    ;dl should be set to the drive number 
+    mov [ebr_drive_number], dl
+    mov ax, 1               ;LBA address
+    mov cl, 1               ;read 1 sector
+    mov bx, 0x7e00          ;put data after bootloader
+    call disk_read
+
     ;print msg
     mov si, msg_akunamatata
     call puts
 
+    cli
     hlt
-
-.halt:
-    jmp .halt
 
 
 ;===================================================================================
 
+;Disk routines
+
+;=================================
+
+;lba address to chs address
+;params:    ax - LBA address
+;returns:   cx - sector (bits 0-5)
+;           cx - cylinder (bits 6-15)
+;           dh - head
+lba_to_chs:
+    push ax
+    push dx
+
+    mov dx, 0
+    div word [bdb_sectors_per_track]        ;ax = LBA / sectors_per_track
+                                            ;dx = LBA % sectors_per_track
+    inc dx
+    mov cx, dx                              ;cx=sector
+    
+    mov dx, 0
+    div word [bdb_heads]                    ;ax = LBA / sectors_per_track / heads = cylinder
+
+    mov dh, dl                              ;dh = head
+    mov ch, al                              ;ch = cylinder
+
+    shl ah, 6                               ;shift left by 6 bits
+    or cl, ah                               ;put upper 2 bits of cylinder in cl
+
+    pop ax
+    mov dl, al
+    pop ax
+
+    ret
+
+;read sectors_from_disk
+;params:    ax = LBA address
+;           cl = num sectors to read (max 128)
+;           es:bx = where to store resulting data
+
+disk_read:
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
+
+    push cx
+    call lba_to_chs
+    pop ax                                  ;al = number of sectors to read
+    
+    mov ah, 0x2
+    mov di, 3                               ;number of read tries to perform
+
+    .retry_read:
+        pusha                               ;don't know what bios sets so save everything
+        stc                                 ;set carry flag
+        int 0x13                            ;carry flag clear = success
+        jnc .done_read
+
+        ;if reach here, read failed
+        popa
+
+        call disk_reset
+        dec di
+        test di, di                         ;if di is 0
+        jnz .retry_read
+
+    .fail_read:
+        ;if reach here, all read tries failed
+        jmp floppy_error
+
+    .done_read:
+        popa
+
+        pop di
+        pop dx
+        pop cx
+        pop bx
+        pop ax
+
+        ret
+
+;resets disk controller
+;params:    dl = drive number
+disk_reset:
+    pusha
+    mov ah, 0
+    stc
+    int 0x13
+    jc floppy_error
+    popa
+    ret
+
+;======================================================================================
 
 msg_akunamatata: db "akunamatataragazzi", ENDL, 0
+msg_read_failed: db "floppy read failed!", ENDL, 0
 
 times 510-($-$$) db 0       ;DIRECTIVE: fill the program with 0s for the first sector (512 bytes)
 dw 0AA55h                   ;
